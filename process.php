12691ac6f877c77f38b732611bfd70360e97a918
Jørgen Jacobsen <jorgeja@gmail.com>
2012-11-21 16:19:58 +0100

    Added support for custom callback url

--- process.php
+++ process.php
@@ -11,7 +11,8 @@ if (isset($_REQUEST['oauth_token']) && $_SESSION['token']  !== $_REQUEST['oauth_
 
 	// if token is old, distroy any session and redirect user to auth.php
 	session_destroy();
-	header('Location: ./auth.php');
+	$headerRedirectToCallbackUrl = 'location: ' . $_SESSION['callbackUrl'];
+	header($headerRedirectToCallbackUrl);
 	
 }elseif(isset($_REQUEST['oauth_token']) && $_SESSION['token'] == $_REQUEST['oauth_token']) {
 
@@ -35,7 +36,8 @@ if (isset($_REQUEST['oauth_token']) && $_SESSION['token']  !== $_REQUEST['oauth_
 		// unset no longer needed request tokens
 		unset($_SESSION['token']);
 		unset($_SESSION['token_secret']);
-		header('Location: ./auth.php');
+		$headerRedirectToCallbackUrl = 'location: ' . $_SESSION['callbackUrl'];
+		header($headerRedirectToCallbackUrl);
 	}else{
 		die("error, try again later!");
 	}
@@ -50,12 +52,16 @@ if (isset($_REQUEST['oauth_token']) && $_SESSION['token']  !== $_REQUEST['oauth_
 
 	//fresh authentication
 	$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
-	$request_token = $connection->getRequestToken(OAUTH_CALLBACK);
-	
+	if($_GET['callbackUrl']) {
+		$callbackUrl = $_GET['callbackUrl']
+	} else {
+		$callbackUrl = OAUTH_CALLBACK;
+	}
+		$request_token = $connection->getRequestToken(OAUTH_CALLBACK);
 	//received token info from twitter
 	$_SESSION['token'] 			= $request_token['oauth_token'];
 	$_SESSION['token_secret'] 	= $request_token['oauth_token_secret'];
-	
+	$_SESSION['callbackUrl'] = $callbackUrl;
 	
 	// any value other than 200 is failure, so continue only if http code is 200
 	if($connection->http_code=='200')
